a25fa28b2d315c2968df2d3e3be77ac3be35d71b
Legend <85244055+LegendGamer885@users.noreply.github.com>
2021-10-29 20:08:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyBcgwVl71fz84gmfS6baI8Pde_u45HqpU8",
+    authDomain: "voting-app-2be84.firebaseapp.com",
+    databaseURL: "https://voting-app-2be84-default-rtdb.firebaseio.com",
+    projectId: "voting-app-2be84",
+    storageBucket: "voting-app-2be84.appspot.com",
+    messagingSenderId: "336014526356",
+    appId: "1:336014526356:web:fb64648280b2cf7fbbf181",
+    measurementId: "G-Z5DLE1ENLH"
+  };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
